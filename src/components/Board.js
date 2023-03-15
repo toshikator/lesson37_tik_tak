01500c2574a316a9d99e79d5053164c1b8ac05ec
Anton Bukhman <diamondspb2015@gmail.com>
2023-03-15 20:25:12 +0200

    ready

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,33 +1,61 @@
 import React, {Component} from 'react';
 import Square from "./Square";
+import {lines} from "./constants/constants";
 
 class Board extends Component {
     constructor(props) {
         super(props);
-        this.state= {
+        this.state = {
             squares: Array(9).fill(null),
             isNext: true // true => 'X', false => 'O'
         }
+        this.count = 0;
+        this.isWinner = null;
     }
 
     fillSquare = (index) => {
-        let temp  = [...this.state.squares];
-        temp[index] = this.state.isNext? 'X' : 'O';
-        this.setState({
-            squares: temp,
-            isNext: !this.state.isNext
-        })
+        if (!this.state.squares[index] && !this.isWinner) {
+            let temp = [...this.state.squares];
+            temp[index] = this.state.isNext ? 'X' : 'O';
+            this.setState({
+                squares: temp,
+                isNext: !this.state.isNext
+            })
+            this.count++;
+        }
+    }
+
+
+    calculateWinner = () => {
+        const squares = [...this.state.squares];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[b] === squares[a] && squares[c] === squares[a]) {
+                return squares[a];
+            }
+        }
     }
 
     renderSquare = index => {
-        return(
-                <Square value={this.state.squares[index]}
-                        fillSquare={this.fillSquare} index={index}/>
-            )
+        return (
+            <Square value={this.state.squares[index]}
+                    fillSquare={this.fillSquare} index={index}/>
+        )
     }
 
     render() {
-        let status = `Next player: ${this.state.isNext? 'X' : 'O'}`
+        if (this.count > 4) {
+            this.isWinner = this.calculateWinner();
+        }
+        let status;
+        if (this.isWinner) {
+            status = `Winner ${this.isWinner}`
+        } else if (this.count === 9) {
+            status = 'draw'
+        } else {
+            status = `Next player: ${this.state.isNext ? 'X' : 'O'}`
+        }
+
         return (
             <div>
                 <div className={'status'}>{status}</div>
